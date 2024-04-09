50e9ab85d1e732a72eec73acb94d31af32658c7e
bruno <brunovicente.dev@gmail.com>
2024-04-09 20:08:41 -0300

    Added types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,17 +16,17 @@ export class AppComponent {
   termoBuscado: string = '';
 
   pokedex = [
-    new Pokemon('Bulbasaur', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'),
-    new Pokemon('Ivysaur', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png'),
-    new Pokemon('Venusaur', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png'),
-    new Pokemon('Charmander', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png'),
-    new Pokemon('Charmeleon', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png'),
-    new Pokemon('Charizard', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png'),
-    new Pokemon('Squirtle', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png'),
-    new Pokemon('Wartortle', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/8.png'),
-    new Pokemon('Blastoise', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/9.png'),
-    new Pokemon('Caterpie', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/10.png'),
-    new Pokemon('Metapod', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/11.png'),
-    new Pokemon('Butterfree', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/12.png'),
+    new Pokemon('Bulbasaur', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png', ['planta', 'veneno']),
+    new Pokemon('Ivysaur', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png', ['planta', 'veneno']),
+    new Pokemon('Venusaur', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png', ['planta', 'veneno']),
+    new Pokemon('Charmander', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png', ['fogo']),
+    new Pokemon('Charmeleon', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png', ['fogo']),
+    new Pokemon('Charizard', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png', ['fogo', 'voador']),
+    new Pokemon('Squirtle', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png', ['agua']),
+    new Pokemon('Wartortle', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/8.png', ['agua']),
+    new Pokemon('Blastoise', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/9.png', ['agua']),
+    new Pokemon('Caterpie', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/10.png', ['inseto']),
+    new Pokemon('Metapod', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/11.png', ['inseto']),
+    new Pokemon('Butterfree', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/12.png', ['inseto', 'voador']),
   ]
 }
--- src/model/pokemonModel.ts
+++ src/model/pokemonModel.ts
@@ -1,9 +1,11 @@
 export class Pokemon {
   name: string;
   image: string;
+  types: string[] = [];
 
-  constructor(name: string, image: string) {
+  constructor(name: string, image: string, types: string[] = []) {
     this.name = name;
     this.image = image;
+    this.types = types;
   }
 }
